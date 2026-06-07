7cd3f302abc6aaa1988559f5d693687ed7438bc9
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-06-07 15:04:56 +0200

    working a bit on yrs. figuring stuff out

--- crdt_test/src/bin/yrs_test.rs
+++ crdt_test/src/bin/yrs_test.rs
@@ -5,11 +5,14 @@
 mod boss_of_yrs;
 use boss_of_yrs::BossOfYrs;
 use serde_json::json;
+use std::sync::Arc;
 use yrs::StateVector;
+use yrs::block::Item;
 use yrs::types::ToJson;
 use yrs::updates::decoder::Decode;
 use yrs::updates::encoder::Encode;
-use yrs::{Doc, GetString, ReadTxn, Text, Transact, Update};
+use yrs::{Array, Doc, GetString, MapPrelim, ReadTxn, Text, TextPrelim, Transact, Update};
+
 /*
  * using this as a reference for figuring out this yrs stuff
  * https://docs.rs/yrs/latest/yrs/#quick-start
@@ -21,38 +24,44 @@ fn main() {
 
     let remote_doc = BossOfYrs::new();
 
-    let text = vec![
-        boss_of_yrs.doc.get_or_insert_text("block1"),
-        boss_of_yrs.doc.get_or_insert_text("block2"),
-    ];
+    let text_blocks = boss_of_yrs.doc.get_or_insert_array("text_blocks");
 
     {
-        let mut txn = boss_of_yrs.doc.transact_mut();
-
-        text[0].insert(&mut txn, 0, "hello");
-        text[0].insert(&mut txn, 5, " world");
-
-        text[1].insert(&mut txn, 0, "bye");
-        text[1].insert(&mut txn, 5, " jupiter");
-
-        /* */
-
         {
-            let mut remote_txn = remote_doc.doc.transact_mut();
-            //stores info about what changes the remote document has and turns into a format that is cheaper to transmit over the network for example.
-            let state_vector = remote_txn.state_vector().encode_v1();
-            let update = txn.encode_diff_v1(&StateVector::decode_v1(&state_vector).unwrap());
-            remote_txn.apply_update(Update::decode_v1(update.as_slice()).unwrap());
+            let mut txn = boss_of_yrs.doc.transact_mut();
+            let block = MapPrelim::from([
+                ("Cat", "MainTitle"),
+                ("text text text", "NormalText"),
+                ("basd sdadas dsadas", "NormalText"),
+            ]);
+
+            let arrayLen = text_blocks.len(&txn);
+            text_blocks.insert(&mut txn, arrayLen, block);
+            //can use push back isntead. fix later
         }
-
+        ///this was for updating and stuff i think?
+        /*
+                {
+                    let mut remote_txn = remote_doc.doc.transact_mut();
+                    //stores info about what changes the remote document has and turns into a format that is cheaper to transmit over the network for example.
+                    let state_vector = remote_txn.state_vector().encode_v1();
+                    let update = txn.encode_diff_v1(&StateVector::decode_v1(&state_vector).unwrap());
+                    remote_txn.apply_update(Update::decode_v1(update.as_slice()).unwrap());
+                }
+        */
         {
-            let map = remote_doc.doc.transact();
-            let map = map.root_refs();
+            let mut txn = boss_of_yrs.doc.transact();
+            let read_array_from_doc = txn.get_array("text_blocks");
+            let read_array_from_doc = read_array_from_doc.unwrap();
+            let read_array_from_doc = read_array_from_doc.iter(&txn);
 
-            println!(
-                "{:?}",
-                map.into_iter().map(|(k, v)| k).collect::<Vec<&str>>()
-            );
+            let mut tmp: String = "".to_string();
+            for x in read_array_from_doc {
+                let x = x.to_string(&txn);
+                tmp.push_str(&x);
+            }
+
+            println!("{:?}", tmp);
             //println!("{}", text[0].get_string(&boss_of_yrs.doc.transact()));
         }
     }
@@ -65,5 +74,4 @@ fn main() {
     //println!("{}", text[0].get_string(&boss_of_yrs.doc.transact()));
     //println!("{}", text[1].get_string(&boss_of_yrs.doc.transact()));
     //println!("{}", serde_json::to_string_pretty(&map).unwrap());
-    println!("test");
 }
