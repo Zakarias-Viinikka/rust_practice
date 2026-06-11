c7da8eb9e3efd183184e827cb8cf13153ef5df1f
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-06-11 13:55:13 +0200

    ran into some deadlock issue. making progress. the order of how i'm writing the yrs stuffs seems really important.

--- crdt_test/src/bin/yrs_test.rs
+++ crdt_test/src/bin/yrs_test.rs
@@ -1,45 +1,16 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(warnings)]
-
-/*#[path = "../boss_of_yrs.rs"]
-mod boss_of_yrs;
-use boss_of_yrs::BossOfYrs;
-#[path = "../params/yrs_params.rs"]
-mod yrs_params;
-use yrs_params::*;*/
+#![allow(unused)]
 
 use crdt_test::yrs_stuff::boss_of_yrs::*;
 
-/*use serde_json::json;
-use std::sync::Arc;
-use yrs::block::Item;
-use yrs::types::ToJson;
-use yrs::updates::decoder::Decode;
-use yrs::updates::encoder::Encode;
-use yrs::{
-    Array, ArrayRef, Doc, GetString, MapPrelim, ReadTxn, Text, TextPrelim, Transact, Update,
-};
-use yrs::{StateVector, Transaction};*/
-
 fn main() {
     let boss_of_yrs = BossOfYrs::new();
+    let key1 = generate_key();
+    //let key2 = generate_key();
 
     let example_data1 = "mock data".to_string();
     let example_meta_data1 = "type_of_content: title,".to_string();
-    boss_of_yrs.insert_new_block(example_data1, example_meta_data1);
-    /*{
-        /*
-        * InsertNewBlockParams<'a> {
-            doc: &'a Doc,
-            block_content: String,
-            type_of_block: String,
-            data_blocks: &'a ArrayRef,
-        }
-        */
-        //let mock_data: Vec<Text> =
-
-        let arrayLen = text_blocks.len(&txn);
-        text_blocks.insert(&mut txn, arrayLen, block);
-    }*/
+    boss_of_yrs.insert_new_block(example_data1, example_meta_data1, key1);
 }
--- crdt_test/src/yrs_stuff/boss_of_yrs.rs
+++ crdt_test/src/yrs_stuff/boss_of_yrs.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused)]
 
-use crate::yrs_stuff::yrs_params::*;
+use crate::yrs_stuff::{boss_of_yrs, yrs_params::*};
 
 use serde_json::json;
 use std::sync::Arc;
@@ -16,8 +17,10 @@ use yrs::{
 };
 use yrs::{StateVector, Transaction};
 
+use rand::prelude::*;
+
 pub struct BossOfYrs {
-    doc: Doc,
+    pub doc: Doc,
 }
 
 impl BossOfYrs {
@@ -25,54 +28,59 @@ impl BossOfYrs {
         Self { doc: Doc::new() }
     }
 
-    pub fn insert_new_block(&self, block_content: String, block_meta_data: String) {
+    pub fn insert_new_block(
+        &self,
+        block_content: String,
+        block_meta_data: String,
+        block_id: String,
+    ) {
         /*
         let textRefExampleData1 = XmlTextPrelim::new("mock data");
         let textRefExampleData2 = XmlTextPrelim::new("rock sata");
         let metaDataExample1 = XmlTextPrelim::new("type_of_content: title,".to_string());
         let metaDataExample2 = XmlTextPrelim::new("type_of_content: regular text,");
         */
-        let block_content = XmlTextPrelim::new(block_content);
+        {
+            let block_content = XmlTextPrelim::new(block_content);
+            //let block_meta_data = XmlElementPrelim::new(block_meta_data, []);
 
-        //https://docs.rs/yrs/latest/yrs/types/xml/struct.XmlElementRef.html
-        /*
-        pub struct XmlElementPrelim {
-            pub tag: Arc<str>,
-            pub attributes: HashMap<Arc<str>, String>,
-            pub children: Vec<XmlIn>,
+            // IMPORTANT
+            // for some reason, if i put this code after
+            // ...txn =...
+            // then my code deadlocks.
+            let data_blocks = self.doc.get_or_insert_map("text_blocks");
+
+            let mut txn = self.doc.transact_mut();
+            /*match txn {
+            Ok(mut txn) => {
+                */
+            data_blocks.insert(&mut txn, block_id.clone(), block_content);
+            /* }
+                Err(e) => {
+                    dbg!("{}", e);
+                }
+            }*/
+            dbg!(data_blocks.to_json(&mut txn));
         }
-         */
-        //xml element has the feature i want, but the way it wants to store data is just weird for my purposes but i don't think there's an easier alternative? it wants an iter so i'll just pass in an empty block.
-        let block_meta_data = XmlElementPrelim::new(block_meta_data, []);
-
-        //XmlTextPrelim::new("a textref mock data");
-        //text_blocks is the id for the 'struct that holds all of the blocks' or something like that.
-        let mut txn = self.doc.transact_mut();
-
         /*
-        i think yrs requires me to store the "text data" in a different  xml type than the metadat if i want them to be synced in different ways. and then the way i sync them is by keeping them in different maps and the way i know what metadata belongs to what block is by using the xmlMap key value.
-        */
-        let data_blocks = self.doc.get_or_insert_map("text_blocks");
-        data_blocks.push_back(&mut txn, block_content);
-
         let data_blocks_metadata = self.doc.get_or_insert_map("text_blocks_metadata");
-        data_blocks_metadata.push_back(&mut txn, block_meta_data);
-
-        //
-        // // this is how far i got ^^^
-        // // idk if it works. but the rust analyzer isn't giving me errors. im tired so im gonna stop for now.
-        //
-        /*
-        block.insert(&mut txn, type_of_block, TextPrelim::new(block_content));*/
-
-        /*
-        let mut txn = doc.transact_mut();
-
-        let block = blocks.push_back(&mut txn, MapPrelim::default());
-        block.insert(&mut txn, "type", "paragraph");
-        let text = block.insert(&mut txn, "content", TextPrelim::new("Hello world"));
+        data_blocks_metadata.insert(&mut txn, block_id, block_meta_data);
         */
+
+        println!("test");
     }
 }
 
 fn println_everything_doc_holds() {}
+
+pub fn generate_key() -> String {
+    let mut rng = rand::rng();
+    let rnd_something: u64 = rng.random();
+    dbg!(rnd_something);
+    return rnd_something.to_string();
+
+    //todo. just a temp gen for now
+    /*
+    i need to create a "generate new key method", but now i create the problem of 2 offline users working on the same page, having the small mathematical probability of generating the same key for 2 different blocks
+    */
+}
