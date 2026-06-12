f92b5040bd961eab2ede7f415348fe037cf1a928
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-06-12 12:49:42 +0200

    playin around with iced. gonna start trying to use this page: https://github.com/iced-rs/iced/tree/latest/examples n\ for trying to learn how stuff works. i found the 'main' guide and the book pretty confusing.

--- gui_for_adding_volume_metadata_to_audio_file/src/main.rs
+++ gui_for_adding_volume_metadata_to_audio_file/src/main.rs
@@ -1,1 +1,29 @@
-pub fn main() -> () {}
+use iced::widget::{column, container, row};
+use iced::{Element, Fill};
+
+pub fn main() -> iced::Result {
+    iced::run(update, view)
+}
+
+fn view(counter: &Counter) -> Element<'_, Message> {
+    container(column!["Top", row!["Left", "Right"].spacing(10), "Bottom"].spacing(10))
+        .padding(10)
+        .center_x(Fill)
+        .center_y(Fill)
+        .into()
+}
+fn update(counter: &mut Counter, message: Message) {
+    match message {
+        Message::Increment => counter.value += 1,
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+#[derive(Default)]
+struct Counter {
+    value: u64,
+}
